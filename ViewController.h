68277cb9f68cecb4bfc40c7bff79ab3db4017eaa
acumulador <jcsalazarm@gmail.com>
2014-09-26 11:42:17 -0500

    Primer Commit
    Primer commit en coronometro

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PasaTiempo
+//
+//  Created by Juan C Salazar on 26/09/14.
+//  Copyright (c) 2014 Juan C Salazar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PasaTiempo
+//
+//  Created by Juan C Salazar on 26/09/14.
+//  Copyright (c) 2014 Juan C Salazar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
